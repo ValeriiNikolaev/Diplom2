545460fd64383720508775649bba68266d845629
Valerii_Nikolaev <crazyrok2009@yandex.ru>
2023-04-08 16:53:24 +0300

    added orders tests

--- src/test/java/ru/practicum/yandex/order/GetOrderTest.java
+++ src/test/java/ru/practicum/yandex/order/GetOrderTest.java
@@ -1,6 +1,66 @@
 package ru.practicum.yandex.order;
 
 
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ru.practicum.yandex.model.order.Order;
+import ru.practicum.yandex.model.order.OrderSteps;
+import ru.practicum.yandex.model.user.User;
+import ru.practicum.yandex.model.user.UserClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.http.HttpStatus.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class GetOrderTest {
+    Order order;
+    OrderSteps orderSteps;
+    User user;
+    UserClient userClient;
+    boolean isDelete;
+    String accessToken;
+    List actualNumber = new ArrayList<>();
+    List numberOrder = new ArrayList<>();
+
+    @Before
+    public void start() throws IndexOutOfBoundsException {
+        user = User.getRegister();
+        userClient = new UserClient();
+        order = Order.getOrder();
+        orderSteps = new OrderSteps();
+        accessToken = userClient.create(user)
+                .statusCode(SC_OK).extract().path("accessToken");
+        numberOrder.add(0,orderSteps.create(order,accessToken)
+                .statusCode(SC_OK).extract().path("order.number"));
+    }
+
+    @After
+    public void finish() {
+        isDelete = userClient.delete(accessToken)
+                .statusCode(SC_ACCEPTED).extract().path("success");
+        assertTrue(isDelete);
+    }
+
+    @Test
+    @DisplayName("Get order authorization user")
+    public void getOrderAuthorizationUserTest() {
+        actualNumber.add(orderSteps.getOrders(accessToken)
+                .statusCode(SC_OK).extract().path("orders.number"));
+        assertEquals(actualNumber.get(0),numberOrder);
+    }
+
+    @Test
+    @DisplayName("Get order not authorization user")
+    public void getOrderNotAuthorizationUserTest() {
+        String error = orderSteps.getOrders("").statusCode(SC_UNAUTHORIZED)
+                .extract().path("message");
+        assertEquals("You should be authorised", error);
+    }
+
 
 }
--- src/test/java/ru/practicum/yandex/order/OrderTest.java
+++ src/test/java/ru/practicum/yandex/order/OrderTest.java
@@ -1,8 +1,83 @@
 package ru.practicum.yandex.order;
 
 
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Before;
+import org.junit.Test;
 import ru.practicum.yandex.model.order.Order;
+import ru.practicum.yandex.model.order.OrderSteps;
+import ru.practicum.yandex.model.user.User;
+import ru.practicum.yandex.model.user.UserClient;
+import ru.practicum.yandex.model.user.UserLogin;
+
+import static org.apache.http.HttpStatus.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class OrderTest {
     Order order;
+    OrderSteps orderSteps;
+    User user;
+    UserClient userClient;
+    String accessToken;
+    boolean isCreate;
+    boolean isDelete;
+
+
+    @Before
+    public void start() {
+        order = Order.getOrder();
+        orderSteps = new OrderSteps();
+        userClient = new UserClient();
+        user = User.getRegister();
+        isCreate = userClient.create(user)
+                .statusCode(SC_OK).extract().path("success");
+        accessToken = userClient.login(UserLogin.getLogin(user))
+                .statusCode(SC_OK).extract().path("accessToken");
+        assertTrue(isCreate);
+    }
+
+    @Test
+    @DisplayName("Creating an order with ingredients and authorization")
+    public void creatingOrderWithIngredientsAuthorizationTest() {
+        String name = orderSteps.create(order, accessToken)
+                .statusCode(SC_OK).extract().path("name");
+        assertEquals("Флюоресцентный бургер", name);
+        isDelete = userClient.delete(accessToken)
+                .statusCode(SC_ACCEPTED).extract().path("success");
+        assertTrue(isDelete);
+    }
+
+    @Test
+    @DisplayName("Creating an order not ingredients")
+    public void creatingOrderNoIngredientsTest() {
+        order.setIngredients(new String[]{});
+        String error = orderSteps.create(order,accessToken)
+                .statusCode(SC_BAD_REQUEST).extract().path("message");
+        assertEquals("Ingredient ids must be provided", error);
+        isDelete = userClient.delete(accessToken)
+                .statusCode(SC_ACCEPTED).extract().path("success");
+        assertTrue(isDelete);
+    }
+
+    @Test
+    @DisplayName("Creating an order not authorization")
+    public void notAuthorizationCreateOrderTest() {
+        String name = orderSteps.create(order,"")
+                .statusCode(SC_OK).extract().path("name");
+        assertEquals("Флюоресцентный бургер", name);
+    }
+
+    @Test
+    @DisplayName("Creating an order with an incorrect hash of ingredients")
+    public void creatingOrderWithIncorrectHashIngredients() {
+        order.setIngredients(new String[]{"no valid"});
+        orderSteps.create(order,accessToken).statusCode(SC_INTERNAL_SERVER_ERROR);
+        isDelete = userClient.delete(accessToken)
+                .statusCode(SC_ACCEPTED).extract().path("success");
+        assertTrue(isDelete);
+    }
+
+
+
 }
