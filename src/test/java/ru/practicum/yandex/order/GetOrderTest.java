a96f4a8fb88237cd47c9ca2b06fc900f5dbcb89c
Valerii_Nikolaev <crazyrok2009@yandex.ru>
2023-04-08 14:49:58 +0300

    added user tests + change model

---
+++ src/test/java/ru/practicum/yandex/order/GetOrderTest.java
@@ -0,0 +1,5 @@
+package ru.practicum.yandex;
+
+public class GetOrderTest {
+
+}
---
+++ src/test/java/ru/practicum/yandex/order/OrderTest.java
@@ -0,0 +1,8 @@
+package ru.practicum.yandex;
+
+
+import ru.practicum.yandex.model.order.Order;
+
+public class OrderTest {
+    Order order;
+}
